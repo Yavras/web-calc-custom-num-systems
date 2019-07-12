149bb530b07e9989c6dc0c358705678e4c80dd95
Yavras <226085@student.pwr.edu.pl>
2019-07-12 16:38:22 +0200

    Init commit

---
+++ src/main/java/com/cnscalc/webcalc/WebCalcCustomNumSystemsApplication.java
@@ -0,0 +1,13 @@
+package com.cnscalc.webcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCalcCustomNumSystemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebCalcCustomNumSystemsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cnscalc/webcalc/WebCalcCustomNumSystemsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cnscalc.webcalc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebCalcCustomNumSystemsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
